c812dbc4a3384ddef58a4ce5755062ce2344468e
Hermes2202 <Hermes2202@gmail.com>
2016-04-01 17:50:13 +0300

    Create Method.java

---
+++ Method.java
@@ -0,0 +1,14 @@
+
+public class Method {
+
+	public static void main(String[] args) {
+		Hello();
+		
+		}
+	static String name = "Piter";
+	static void Hello(){
+		
+		System.out.println("Hello "+name);
+		}
+
+	}
