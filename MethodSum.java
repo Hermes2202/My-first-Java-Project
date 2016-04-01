11f54541626d5cb08f655d2479dbc109df07c4c4
Hermes2202 <Hermes2202@gmail.com>
2016-04-01 19:38:15 +0300

    Create MethodSum.java

---
+++ MethodSum.java
@@ -0,0 +1,20 @@
+
+public class MethodSum {
+
+	
+	public static void main(String[] args) {
+	
+	obchayaSum(25+75);
+	}
+	static int num1 = 25;
+	static int num2 = 75;
+	
+	static void obchayaSum(int obchayaSum) {
+		
+		
+		System.out.println(obchayaSum);
+		System.out.println(num2);
+		System.out.println(num1);
+	}
+
+}
