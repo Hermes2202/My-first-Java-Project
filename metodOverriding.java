bbcf1ceac37d456cee0759ffa8250fb01c0600b2
Hermes2202 <Hermes2202@gmail.com>
2016-04-02 15:04:06 +0300

    Create metodOverriding.java

---
+++ metodOverriding.java
@@ -0,0 +1,19 @@
+
+public class MethodSquer {
+
+	public static void main(String[] args) {
+		
+	System.out.println(square(2));
+	System.out.println(square(5));
+	System.out.println(square(10));
+	System.out.println(square(87.6));
+	System.out.println(square(8763.3));
+	}
+	static double square(double number){//оздаем метод square c double//
+		return number*number; //возвращаем значение переменной number * number//
+	}
+     static int square(int number){
+     return number*number;
+    }
+    
+}
