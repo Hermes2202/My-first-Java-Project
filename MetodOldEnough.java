260d4802ec1fa0a2e9ef6e002d0208b3b0a14100
Hermes2202 <Hermes2202@gmail.com>
2016-04-03 20:19:26 +0300

    Create MetodOldEnough.java

---
+++ MetodOldEnough.java
@@ -0,0 +1,16 @@
+
+public class MetodOldEnough {
+
+	public static void main(String[] args) {
+		
+		int age = 31; // sozdali peremennuyu age vozrast
+		if ( age >= 21);
+		System.out.println((age >=21)); 
+	}
+	
+	static boolean oldEnough(boolean age){      
+	return age;                                 
+	// Вроде как то так )
+    }
+    
+}
