69bab1d106e4431fccc6e84286f0fad64bcd5ea5
Hermes2202 <Hermes2202@gmail.com>
2016-04-06 21:48:58 +0300

    Create lohikaMin.java

---
+++ lohikaMin.java
@@ -0,0 +1,18 @@
+
+public class LohikaMin {
+
+	public static void main(String[] args) {
+
+	System.out.println(min(10,20,30));
+	
+	
+	}
+		static boolean min(int a, int b, int c){
+		
+		if (a < b);
+		if (a < c);
+		return true;
+		
+	 }
+
+}
