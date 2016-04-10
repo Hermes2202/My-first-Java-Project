5f3100f3b57b9347348fea878c7de680fd658a95
Hermes2202 <Hermes2202@gmail.com>
2016-04-10 19:14:08 +0300

    Create TankiNachalo.java

---
+++ TankiNachalo.java
@@ -0,0 +1,34 @@
+
+public class TankiNachalo {
+
+	public static void main(String[] args) {
+		System.out.print(getQuadrant(" 1"));
+		System.out.print(getQuadrant("2"));
+		System.out.print(getQuadrant("3"));
+		System.out.print(getQuadrant("4"));
+		System.out.print(getQuadrant("5"));
+		System.out.print(getQuadrant("6"));
+		System.out.print(getQuadrant("7"));
+		System.out.print(getQuadrant("8"));
+		System.out.print(getQuadrant("9"));
+		
+		System.out.println();
+		System.out.println(getQuadrant("a"));
+		System.out.println(getQuadrant("b"));
+		System.out.println(getQuadrant("c"));
+		System.out.println(getQuadrant("d"));
+		System.out.println(getQuadrant("e"));
+		System.out.println(getQuadrant("f"));
+		System.out.println(getQuadrant("g"));
+		System.out.println(getQuadrant("h"));
+		System.out.println(getQuadrant("i"));
+		//Если честно я не совсем понял что и как нам это сделать
+		
+	}
+	static String getQuadrant(String v) {
+		return v;
+		 
+		
+	}
+	
+}
