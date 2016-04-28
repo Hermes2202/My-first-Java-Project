acf03959f6b3ad511587ef368a969b408aeb07cc
Hermes2202 <Hermes2202@gmail.com>
2016-04-28 23:48:58 +0300

    Create Loop1.java

---
+++ Loop1.java
@@ -0,0 +1,16 @@
+
+public class Loop1 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while (i<10){
+			if(i<=10){
+			i += 1;
+			if(i == 10){
+			break;
+			}
+		}
+			System.out.println(i); 
+	   }
+	}
+}
