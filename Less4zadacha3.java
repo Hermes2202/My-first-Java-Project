34ada489d24f61235c3c1287ae1581feb6e80561
Piter Duginov <Hermes2202@gmail.com>
2016-05-21 20:09:10 +0300

    Create Less4zadacha3.java

---
+++ Less4zadacha3.java
@@ -0,0 +1,26 @@
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+//Заполнить массив числами от 100 до 0.
+public class Less4zadacha3 {
+    public static void main(String[] args) {
+        int [] a = new int [50];
+
+        for(int i =0; i < a.length; ++i){
+            if(i == 0) {
+                a[i] = 1;
+            }else{
+                a[i] = a[i -1] +2;
+            }
+        }
+        for(int x: a) {
+            System.out.println(x + " ");
+        }
+        System.out.println();
+            for(int i = a.length -1; i >=0; --i){
+                System.out.print(a[i]+" ");
+            }
+        }
+    }
+
