46e88d05fa67669d61d0d3c00aa39b6f7b8c681a
Piter Duginov <Hermes2202@gmail.com>
2016-05-21 22:36:43 +0300

    Update Less4zadacha3.java

--- Less4zadacha3.java
+++ Less4zadacha3.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 //Заполнить массив числами от 100 до 0.
 public class Less4zadacha3 {
     public static void main(String[] args) {
-        int [] a = new int [50];
+        int [] a = new int [100];
 
-        for(int i =0; i < a.length; ++i){
+        for(int i =0; i < a.length; i++){
             if(i == 0) {
                 a[i] = 1;
             }else{
-                a[i] = a[i -1] +2;
+                a[i] = a[i -1] +1;
             }
         }
         for(int x: a) {
